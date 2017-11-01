a7a638393e66cbb8592864d838d3b31a9303fd0d
Monzoor Morshed Mahmud <mmtamal@gmail.com>
2017-11-01 10:46:25 +0600

    * Art page init

--- assets/js/pages/product_details/04_product_details.js
+++ assets/js/pages/product_details/04_product_details.js
@@ -1,119 +1,143 @@
 
 (function ($) {
-    $(document).ready(function () {
-        var verticalCarousel = {
-            // log: false,
-            fx: "carousel",
-            carouselVertical: true,
-            carouselVisible: 4,
-            allowWrap: false,
-            timeout: 0,
-            next: "#next4",
-            prev: "#prev4",
-            // pager: "#pager4",
-            // slides: "> div"
-        }
+    var verticalCarousel = {
+        // log: false,
+        fx: "carousel",
+        carouselVertical: true,
+        carouselVisible: 4,
+        allowWrap: false,
+        timeout: 0,
+        next: "#next4",
+        prev: "#prev4",
+        // pager: "#pager4",
+        // slides: "> div"
+    }
 
-        var horizontalCarousel = {
-            // log: false,
-            fx: "carousel",
-            carouselVertical: false,
-            carouselVisible: 4,
-            allowWrap: false,
-            timeout: 0,
-            swipe: true,
-            next: "#next4",
-            prev: "#prev4",
-            pager: "#pager4",
-            carouselFluid: true,
-            autoHeight: "3:1",
-            // slides: "> div"
-        }
-        var mainSlider = {
-            log: false,
-            fx: "scrollHorz",
-            allowWrap: false,
-            timeout: 0,
-            swipe: true,
-            prev: "#prev1",
-            next: "#next1",
-            slides: "> div",
-            autoHeight: "10:13",
-        }
+    var horizontalCarousel = {
+        // log: false,
+        fx: "carousel",
+        carouselVertical: false,
+        carouselVisible: 4,
+        allowWrap: false,
+        timeout: 0,
+        swipe: true,
+        next: "#next4",
+        prev: "#prev4",
+        pager: "#pager4",
+        carouselFluid: true,
+        autoHeight: "3:1",
+        // slides: "> div"
+    }
 
 
 
-        var responsiveThumbSlider = function (winSize,isMobile) {
-            if(isMobile) {
-                $('.main-slider-wrapper').addClass('div-center');
-                $('.slideshow').cycle('destroy');
-                $(".slideshow").cycle(horizontalCarousel);
-                return;
-            }
-            $('.main-slider-wrapper').removeClass('div-center');
+    var responsiveThumbSlider = function (winSize,isMobile) {
+        if(isMobile) {
+            $('.main-slider-wrapper').addClass('div-center');
             $('.slideshow').cycle('destroy');
-            $(".slideshow").cycle(verticalCarousel)
+            $(".slideshow").cycle(horizontalCarousel);
+            return;
         }
+        $('.main-slider-wrapper').removeClass('div-center');
+        $('.slideshow').cycle('destroy');
+        $(".slideshow").cycle(verticalCarousel)
+    }
 
 
-        var getWindowWidth = function () {
-            return $(window).innerWidth();
-        }
+    var getWindowWidth = function () {
+        return $(window).innerWidth();
+    }
 
-        var isMobile = function (){
-            return (getWindowWidth()< 992)?true:false;
-        }
-        var initSlider = function (){
-            responsiveThumbSlider(getWindowWidth(),isMobile());
-        }
+    var isMobile = function (){
+        return (getWindowWidth()< 992)?true:false;
+    }
+    var initSlider = function (){
+        responsiveThumbSlider(getWindowWidth(),isMobile());
+    }
 
-        var resizer =  function () {
-            var resize = false;
-            var cachedWidth = $(window).width();
-            $(window).resize(function(){
-                var newWidth = $(window).width();
-                if(newWidth !== cachedWidth){
-                    if (resize) {
-                        clearTimeout(resize);
-                    }
-                    resize = setTimeout(function() {
-                        responsiveThumbSlider(getWindowWidth(),isMobile());
-                    }, 400);
-                    cachedWidth = newWidth;
+    var resizer =  function () {
+        var resize = false;
+        var cachedWidth = $(window).width();
+        $(window).resize(function(){
+            var newWidth = $(window).width();
+            if(newWidth !== cachedWidth){
+                if (resize) {
+                    clearTimeout(resize);
                 }
-            });
-        }
+                resize = setTimeout(function() {
+                    responsiveThumbSlider(getWindowWidth(),isMobile());
+                }, 400);
+                cachedWidth = newWidth;
+            }
+        });
+    }
 
-        var mainSliderCreator =  function () {
-            $(".cycle-carousel-wrap img").each(function (i){
-                var imgSrc = $(this).data('image');
-                var imgTag = '<div class="main-img-wrapper" data-thumbid="'+i+'"><img src="'+imgSrc+'"></div>'
-                $('.main-img').append(imgTag);
-            })
-        }
 
-        var mainSliderInit =  function () {
-            $(".main-img").cycle(mainSlider);
-        }
+    var responsiveSlider = function (){
+        initSlider();
+        resizer();
+    }()
 
-        var thumbSelection = function () {
-            $(".cycle-carousel-wrap img").on('click', function (e) {
-                $(".cycle-carousel-wrap img").removeClass('selected');
-                $(this).addClass('selected');
-                $('.main-img').cycle('goto', $(this).data('id'));
-            })
-        }
 
-        var responsiveSlider = function (){
-            initSlider();
-            resizer();
-            mainSliderCreator();
-            mainSliderInit();
-            thumbSelection();
-        }();
 
+
+
+
+
+    // $(".slideshow").cycle(horizontalCarousel)
+    $(".cycle-carousel-wrap img").each(function (i){
+        // console.log('jh',i);
+        var imgSrc = $(this).data('image');
+        var imgTag = '<div class="main-img-wrapper" data-thumbid="'+i+'"><img src="'+imgSrc+'"></div>'
+        // var imgTag = '<img src="'+imgSrc+'">'
+        $('.main-img').append(imgTag);
+    })
+    $(".main-img").cycle({
+        log: false,
+        fx: "scrollHorz",
+        allowWrap: false,
+        timeout: 0,
+        swipe: true,
+        prev: "#prev1",
+        next: "#next1",
+        slides: "> div",
+        autoHeight: "10:13",
+    })
+    $(".cycle-carousel-wrap img").on('click', function (e) {
+        // console.log('okk',$(this).data('id'));
+        $(".cycle-carousel-wrap img").removeClass('selected');
+        $(this).addClass('selected');
+        // $('.slideshow').data("cycle.opts").currSlide
+        // console.log($('.slideshow').data("cycle.opts").currSlide);
+        $('.main-img').cycle('goto', $(this).data('id'));
     })
 
+    // $('#next1, #prev1').on('click', function (){
+    //     // console.log($('.main-img-wrapper').data('thumbid'));
+    //     $('.slideshow').cycle('goto', $('.main-img .cycle-slide-active').data('thumbid'));
+    //     $(".cycle-carousel-wrap img").removeClass('selected');
+    //     if($('.slideshow img').hasClass('cycle-slide-active')) {
+    //         $('.cycle-slide-active').addClass('selected')
+    //     }
+    //
+    //
+    // })
 
+    // var ts;
+    // $(document).bind('touchstart', function (e){
+    //    ts = e.originalEvent.touches[0].clientY;
+    // });
+    //
+    // $(document).bind('touchend', function (e){
+    //    var te = e.originalEvent.changedTouches[0].clientY;
+    //    if(ts > te+5){
+    //
+    //     //   slide_down();
+    //       $('.slideshow').cycle('next');
+    //    }else if(ts < te-5){
+    //     //   slide_up();
+    //       $('.slideshow').cycle('prev');
+    //    }
+    // });
 
 })(window.jQuery);
